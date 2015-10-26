ca8127f3746736f6e8bfcaea0087932f95c34f7f
Timm Friebe <thekid@thekid.de>
2015-10-27 00:39:41 +0100

    Refactor runInternal() to use mirrors

--- src/main/php/unittest/TestSuite.class.php
+++ src/main/php/unittest/TestSuite.class.php
@@ -7,7 +7,7 @@ use lang\IllegalStateException;
 use lang\IllegalArgumentException;
 use lang\XPClass;
 use lang\Throwable;
-use lang\reflect\TargetInvocationException;
+use lang\mirrors\TargetInvocationException;
 use lang\mirrors\TypeMirror;
 
 /**
@@ -23,12 +23,18 @@ class TestSuite extends \lang\Object {
   protected $order= [];
   protected $listeners= [];
 
-  private static $BASE, $TESTS;
+  private static $BASE, $TESTS, $BEFORECLASS, $AFTERCLASS;
 
   static function __static() {
     self::$BASE= new TypeMirror('unittest.TestCase');
     self::$TESTS= newinstance('util.Filter', [], '{
       public function accept($m) { return $m->annotations()->provides("test"); }
+    }');
+    self::$BEFORECLASS= newinstance('util.Filter', [], '{
+      public function accept($m) { return $m->annotations()->provides("beforeClass"); }
+    }');
+    self::$AFTERCLASS= newinstance('util.Filter', [], '{
+      public function accept($m) { return $m->annotations()->provides("afterClass"); }
     }');
   }
 
@@ -211,8 +217,9 @@ class TestSuite extends \lang\Object {
    */
   protected function actionsFor($annotatable, $impl) {
     $r= [];
-    if ($annotatable->hasAnnotation('action')) {
-      $action= $annotatable->getAnnotation('action');
+    $annotations= $annotatable->annotations();
+    if ($annotations->provides('action')) {
+      $action= $annotations->named('action')->value();
       $type= XPClass::forName($impl);
       if (is_array($action)) {
         foreach ($action as $a) {
@@ -233,43 +240,42 @@ class TestSuite extends \lang\Object {
    * @throws  lang.MethodNotImplementedException
    */
   protected function runInternal($test, $result) {
-    $class= $test->getClass();
-    $method= $class->getMethod($test->name);
+    $mirror= new TypeMirror(typeof($test));
+    $method= $mirror->method($test->name);
+    $annotations= $method->annotations();
     $this->notifyListeners('testStarted', [$test]);
     
     // Check for @ignore
-    if ($method->hasAnnotation('ignore')) {
+    if ($annotations->provides('ignore')) {
       $this->notifyListeners('testNotRun', [
-        $result->set($test, new TestNotRun($test, $method->getAnnotation('ignore')))
+        $result->set($test, new TestNotRun($test, $annotations->named('ignore')->value()))
       ]);
       return;
     }
 
     // Check for @expect
     $expected= null;
-    if ($method->hasAnnotation('expect', 'class')) {
-      $message= $method->getAnnotation('expect', 'withMessage');
-      if ('/' === $message{0}) {
-        $pattern= $message;
+    if ($annotations->provides('expect')) {
+      $expect= $annotations->named('expect')->value();
+      if (!isset($expect['withMessage'])) {
+        $expected= [XPClass::forName($expect), null];
+      } else if ('/' === $expect['withMessage']{0}) {
+        $expected= [XPClass::forName($expect['class']), $expect['withMessage']];
       } else {
-        $pattern= '/'.preg_quote($message, '/').'/';
+        $expected= [XPClass::forName($expect['class']), '/'.preg_quote($expect['withMessage'], '/').'/'];
       }
-      $expected= [XPClass::forName($method->getAnnotation('expect', 'class')), $pattern];
-    } else if ($method->hasAnnotation('expect')) {
-      $expected= [XPClass::forName($method->getAnnotation('expect')), null];
     }
     
     // Check for @limit
     $eta= 0;
-    if ($method->hasAnnotation('limit')) {
-      $eta= $method->getAnnotation('limit', 'time');
+    if ($annotations->provides('limit')) {
+      $eta= $annotations->named('limit')->value()['time'];
     }
 
     // Check for @values
-    if ($method->hasAnnotation('values')) {
-      $annotation= $method->getAnnotation('values');
+    if ($annotations->provides('values')) {
       $variation= true;
-      $values= $this->valuesFor($test, $annotation);
+      $values= $this->valuesFor($test, $annotations->named('values')->value());
     } else {
       $variation= false;
       $values= [[]];
@@ -277,7 +283,7 @@ class TestSuite extends \lang\Object {
 
     // Check for @actions, initialize setUp and tearDown call chains
     $actions= array_merge(
-      $this->actionsFor($class, 'unittest.TestAction'),
+      $this->actionsFor($mirror, 'unittest.TestAction'),
       $this->actionsFor($method, 'unittest.TestAction')
     );
     $setUp= function($test) use($actions) {
@@ -479,8 +485,9 @@ class TestSuite extends \lang\Object {
    * @param  lang.XPClass class
    */
   protected function beforeClass($class) {
-    foreach ($class->getMethods() as $m) {
-      if (!$m->hasAnnotation('beforeClass')) continue;
+    $mirror= new TypeMirror($class);
+    foreach ($mirror->methods()->all(self::$BEFORECLASS) as $m) {
+      if (!$m->annotations()->provides('beforeClass')) continue;
       try {
         $m->invoke(null, []);
       } catch (TargetInvocationException $e) {
@@ -488,11 +495,11 @@ class TestSuite extends \lang\Object {
         if ($cause instanceof PrerequisitesNotMetError) {
           throw $cause;
         } else {
-          throw new PrerequisitesNotMetError('Exception in beforeClass method '.$m->getName(), $cause);
+          throw new PrerequisitesNotMetError('Exception in beforeClass method '.$m->name(), $cause);
         }
       }
     }
-    foreach ($this->actionsFor($class, 'unittest.TestClassAction') as $action) {
+    foreach ($this->actionsFor($mirror, 'unittest.TestClassAction') as $action) {
       $action->beforeTestClass($class);
     }
   }
@@ -504,11 +511,11 @@ class TestSuite extends \lang\Object {
    * @param  lang.XPClass class
    */
   protected function afterClass($class) {
-    foreach ($this->actionsFor($class, 'unittest.TestClassAction') as $action) {
+    $mirror= new TypeMirror($class);
+    foreach ($this->actionsFor($mirror, 'unittest.TestClassAction') as $action) {
       $action->afterTestClass($class);
     }
-    foreach ($class->getMethods() as $m) {
-      if (!$m->hasAnnotation('afterClass')) continue;
+    foreach ($mirror->methods()->all(self::$AFTERCLASS) as $m) {
       try {
         $m->invoke(null, []);
       } catch (TargetInvocationException $ignored) { }
