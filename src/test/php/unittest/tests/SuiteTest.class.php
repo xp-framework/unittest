b378f2fc24f2a66ca2da350ffdfbd828c788f8b6
Timm Friebe <thekid@thekid.de>
2016-01-17 14:54:07 +0100

    Fix PHP 5.5 and PHP 5.6

--- src/test/php/unittest/tests/SuiteTest.class.php
+++ src/test/php/unittest/tests/SuiteTest.class.php
@@ -356,6 +356,17 @@ class SuiteTest extends TestCase {
   }
 
   #[@test]
+  public function native_exceptions_make_test_fail() {
+    $test= newinstance(TestCase::class, ['fixture'], [
+      '#[@test] fixture' => function() { throw new \Exception('Test'); }
+    ]);
+    $this->assertInstanceOf(
+      'lang.Error',
+      $this->suite->runTest($test)->failed[$test->hashCode()]->reason
+    );
+  }
+
+  #[@test, @action(new RuntimeVersion('>=7.0.0'))]
   public function native_php7_errors_make_test_fail() {
     $test= newinstance(TestCase::class, ['fixture'], [
       '#[@test] fixture' => function() { $null= null; $null->method(); }
