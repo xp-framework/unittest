1349cc13ec84742a5dbab23fa461daaea684d6dd
Timm Friebe <thekid@thekid.de>
2018-06-24 18:04:03 +0200

    Use ClassLoader::findUri() / loadUri() API...
    ...instead of mapping filenames to classes ourselves. This way, the
    class loading infrastructure will take are of this; and this part
    of the code is way more robust against changes to it

--- src/main/php/xp/unittest/sources/FolderSource.class.php
+++ src/main/php/xp/unittest/sources/FolderSource.class.php
@@ -12,7 +12,7 @@ use lang\IllegalArgumentException;
  * @test  xp://unittest.tests.FolderSourceTest
  */
 class FolderSource extends ClassesSource {
-  private $loader, $package;
+  private $folder;
   
   /**
    * Constructor
@@ -21,31 +21,36 @@ class FolderSource extends ClassesSource {
    * @throws lang.IllegalArgumentException if the given folder does not exist or isn't in class path
    */
   public function __construct(Folder $folder) {
+    if (!$folder->exists()) {
+      throw new IllegalArgumentException($folder->toString().' does not exist');
+    }
+
     $path= $folder->getURI();
     foreach (ClassLoader::getLoaders() as $cl) {
       $l= strlen($cl->path);
       if ($cl instanceof FileSystemClassLoader && 0 === strncmp($cl->path, $path, $l)) {
-        $this->loader= $cl;
-        $this->package= rtrim(strtr(substr($path, $l), [DIRECTORY_SEPARATOR => '.']), '.');
+        $this->folder= $folder;
         return;
       }
     }
 
-    throw new IllegalArgumentException($folder->toString().($folder->exists()
-      ? ' is not in class path'
-      : ' does not exist'
-    ));
+    throw new IllegalArgumentException($folder->toString().' is not in class path');
   }
 
   /** @return iterable */
-  private function classesIn($package) {
-    $e= -strlen(\xp::CLASS_FILE_EXT);
-    $p= $package ? $package.'.' : '';
-    foreach ($this->loader->packageContents($package) as $file) {
-      if (0 === substr_compare($file, \xp::CLASS_FILE_EXT, $e)) {
-        yield $this->loader->loadClass($p.substr($file, 0, $e));
-      } else if ('/' === $file{strlen($file) - 1}) {
-        foreach ($this->classesIn($p.substr($file, 0, -1)) as $class) {
+  private function classesIn($folder) {
+    $cl= ClassLoader::getDefault();
+    foreach ($folder->entries() as $entry) {
+      $name= $entry->name();
+
+      // Must begin with an uppercase letter, and end with ".php"
+      if (1 === strspn($name, 'ABCDEFGIJKLMOPQRSTUVWXYZ') && 0 === substr_compare($name, '.php', -4)) {
+        $uri= $entry->asURI();
+        if ($loader= $cl->findUri($uri)) {
+          yield $loader->loadUri($uri);
+        }
+      } else if ($entry->isFolder()) {
+        foreach ($this->classesIn($entry->asFolder()) as $class) {
           yield $class;
         }
       }
@@ -54,7 +59,7 @@ class FolderSource extends ClassesSource {
 
   /** @return iterable */
   protected function classes() {
-    return $this->classesIn($this->package);
+    return $this->classesIn($this->folder);
   }
 
   /**
@@ -63,6 +68,6 @@ class FolderSource extends ClassesSource {
    * @return string
    */
   public function toString() {
-    return nameof($this).'['.$this->loader->toString().($this->package ? ', package '.$this->package : '').']';
+    return nameof($this).'['.$this->folder->toString().']';
   }
 }
