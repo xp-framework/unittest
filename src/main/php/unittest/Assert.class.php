2c6e035dd6fc74e3cf897afacf94bf2bae4c7809
Timm Friebe <thekid@thekid.de>
2019-10-01 13:44:44 +0200

    Implement default assertions
    See https://github.com/xp-framework/unittest/pull/37#discussion_r329783057

--- src/main/php/unittest/Assert.class.php
+++ src/main/php/unittest/Assert.class.php
@@ -1,8 +1,14 @@
 <?php namespace unittest;
 
+use lang\Type;
 use util\Objects;
 
-class Assert {
+/**
+ * Default assertion set
+ *
+ * @test  xp://unittest.tests.AssertTest
+ */
+abstract class Assert {
 
   /**
    * Assert that two values are equal
@@ -15,6 +21,74 @@ class Assert {
   public static function equals($expected, $actual, $error= 'equals') {
     if (!Objects::equal($expected, $actual)) {
       throw new AssertionFailedError(new ComparisonFailedMessage($error, $expected, $actual));
+    }
+  }
+
+  /**
+   * Assert that two values are not equal
+   *
+   * @param  var expected
+   * @param  var actual
+   * @param  string error default 'equal'
+   * @return void
+   */
+  public static function notEquals($expected, $actual, $error= '!equals') {
+    if (Objects::equal($expected, $actual)) {
+      throw new AssertionFailedError(new ComparisonFailedMessage($error, $expected, $actual));
+    }
+  }
+
+  /**
+   * Assert that a value is true
+   *
+   * @param  var $actual
+   * @param  string $error default '==='
+   * @return void
+   */
+  public static function true($actual, $error= '===') {
+    if (true !== $actual) {
+      throw new AssertionFailedError(new ComparisonFailedMessage($error, true, $actual));
+    }
+  }
+  
+  /**
+   * Assert that a value is false
+   *
+   * @param  var $actual
+   * @param  string $error default '==='
+   * @return void
+   */
+  public static function false($actual, $error= '===') {
+    if (false !== $actual) {
+      throw new AssertionFailedError(new ComparisonFailedMessage($error, false, $actual));
+    }
+  }
+
+  /**
+   * Assert that a value's type is null
+   *
+   * @param  var $actual
+   * @param  string $error default '==='
+   * @return void
+   */
+  public static function null($actual, $error= '===') {
+    if (null !== $actual) {
+      throw new AssertionFailedError(new ComparisonFailedMessage($error, null, $actual));
+    }
+  }
+
+  /**
+   * Assert that a given object is a subclass of a specified class
+   *
+   * @param  string|lang.Type $type
+   * @param  var $actual
+   * @param  string $error default 'instanceof'
+   * @return void
+   */
+  public static function instance($type, $actual, $error= 'instanceof') {
+    $t= $type instanceof Type ? $type : Type::forName($type);
+    if (!$t->isInstance($actual)) {
+      throw new AssertionFailedError(new ComparisonFailedMessage($error, $t->getName(), typeof($actual)->getName()));
     }
   }
 }
---
+++ src/test/php/unittest/tests/AssertTest.class.php
@@ -0,0 +1,89 @@
+<?php namespace unittest\tests;
+
+use unittest\Assert;
+use unittest\AssertionFailedError;
+
+class AssertTest {
+
+  /** @return iterable */
+  private function values() {
+    return [
+      [0], [1], [-1],
+      [''], ['Test'],
+      [true], [false], [null],
+      [[]], [[1, 2, 3]], [['key' => 'value']],
+      [function() { }],
+      [new Value('Test')],
+    ];
+  }
+
+  #[@test, @values('values')]
+  public function values_equal_themselves($value) {
+    Assert::equals($value, $value);
+  }
+
+  #[@test, @values('values')]
+  public function values_do_not_equal_this($value) {
+    Assert::notEquals($this, $value);
+  }
+
+  #[@test]
+  public function true_equals() {
+    Assert::true(true);
+  }
+
+  #[@test]
+  public function false_equals() {
+    Assert::false(false);
+  }
+
+  #[@test]
+  public function null_equals() {
+    Assert::null(null);
+  }
+
+  #[@test]
+  public function instanceof_self() {
+    Assert::instance(self::class, $this);
+  }
+
+  #[@test]
+  public function instanceof_subclass() {
+    Assert::instance(Value::class, newinstance(Value::class, [$this], []));
+  }
+
+  #[@test, @expect(AssertionFailedError::class)]
+  public function equals_raises_error_when_not_equal() {
+    Assert::equals(1, 2);
+  }
+
+  #[@test, @expect(AssertionFailedError::class)]
+  public function notEquals_raises_error_when_equal() {
+    Assert::notEquals(1, 1);
+  }
+
+  #[@test, @expect(AssertionFailedError::class)]
+  public function true_raises_error_when_not_true() {
+    Assert::true(false);
+  }
+
+  #[@test, @expect(AssertionFailedError::class)]
+  public function false_raises_error_when_not_false() {
+    Assert::false(true);
+  }
+
+  #[@test, @expect(AssertionFailedError::class)]
+  public function null_raises_error_when_not_null() {
+    Assert::null($this);
+  }
+
+  #[@test, @expect(AssertionFailedError::class)]
+  public function instanceof_raises_error_when_given_non_object() {
+    Assert::instance(self::class, null);
+  }
+
+  #[@test, @expect(AssertionFailedError::class)]
+  public function instanceof_raises_error_when_given_non_instance() {
+    Assert::instance(Value::class, $this);
+  }
+}
